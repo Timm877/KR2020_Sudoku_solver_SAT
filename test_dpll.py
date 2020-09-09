e704c181c346020caf45da6ab9af6b59d71d79f9
Timm877 <58209499+Timm877@users.noreply.github.com>
2020-09-09 17:44:10 +0200

    make function dimacs_to_cnf and get_dimacs in test_dpll.py & added the DPLL solver. Run test_dpll works for sudoku example

---
+++ test_dpll.py
@@ -0,0 +1,73 @@
+import glob
+import random
+import dpll
+
+def dimacs_to_cnf(full_problem):
+  '''This function converts a dimacs file to a cnf statement'''
+
+  clauses = []
+  lines = full_problem.split("\n")
+  for line in lines:
+    if len(line) == 0 or line[0] in ['c', 'p', '%', '0']:
+      continue
+    clause = [int(x) for x in line.split()[0:-1]]
+    clauses.append({x for x in clause})
+  return clauses
+
+
+def get_dimacs():
+  
+  '''This function reads sudoku9x9.txt (txtfile with 1000 9x9 sudoku's) and will translate it to DIMACS
+  Every point in that file equals 0; every number is a given number in the sudoku
+  So 1 9x9 sudoku are the first 81 characters in the file.
+  It also reads the rules of sudoku as Dimacs
+
+  Output of the function is a big dimacs file containing rules + sudoku example'''
+
+  # Get the first sudoku out of the txt file with 1000 9x9 sudoku's 
+  sudokus_9x9_string = open('sudokus9x9.txt', 'r').read()
+  first_sudoku = sudokus_9x9_string[:81]
+
+  # TODO: 
+  # convert first_sudoku string into dimacs format
+  # for every ., make a line with [rownum colnum 0 0]
+  # for every number, make a line with [rownum colnum number 0]
+  
+
+  # we have 1 example Dimacs file from Canvas!
+  example_sud_dimacs = open('sudoku_example_DIMACS.txt', 'r').read()
+
+
+  # load in the constraints / rules
+  # (1 independent number in every place, 1 independent number in every row, every column and every square)
+  rules_string = open('sudoku_rules_DIMACS.txt', 'r').read()
+  
+
+  # rules_string is already in DIMACS, no need to change anything for it.
+  # we want to solve a combination of the constraint with the given numbers of the sudoku with DPLL
+  # so I think our cnf problem statement should be: rules + given_numbers
+  # full_problem = rules_string + sudoku_given numbers
+  # for the example sudoku:
+  full_problem = example_sud_dimacs + rules_string
+
+  return full_problem
+
+
+
+# here we can call dpll, or GSAT, or Jeroslaw for our solution:
+# currently, we only have a dpll function
+full_problem1 = get_dimacs()
+clauses1 = dimacs_to_cnf(full_problem1)
+solution_dpll = dpll.Solver(clauses1)
+
+#TODO: get the output sudoku..
+
+# uncomment the line below to see that this gives as output True, so the sudoku is solved
+# program is however sloooooow to give answer to this statement
+#print(solution_dpll.solve() == True)
+
+
+
+
+
+
