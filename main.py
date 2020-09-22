daf9d2f6fff62b1355d347619862a0eaf2bc8a98
daniyal9538 <37014621+daniyal9538@users.noreply.github.com>
2020-09-22 18:22:25 +0500

    more fixes

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('input_file', help = 'Path to file with SAT problem encoded as CNF in DIMACS format')
 
-    parser.add_argument('-r', '--rules', help = 'Path to file with rules for Sudoku encoded in DIMACS, default file is provided',
+    parser.add_argument('-r', '--rules', help = 'Path to file with rules for Sudoku encoded in DIMACS, default file is provided' +
+                        '\nIf you wish to not pass in a rule files, set argument to none',
                         default='sudoku_rules_DIMACS.txt')
     parser.add_argument('-hr', '--branching_heuristic',
                          help=f'Branching Heuristic to be used with DPLL. Options are: {heuristics_dict}',
